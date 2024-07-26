6d8a94f55128bda0912831b86100d4be88f51e7f
Yogeshwar Jamdade <yogeshwar473@gmail.com>
2024-07-26 09:08:20 +0530

    Making microservices Docker compatible

---
+++ Section04/loans/src/test/java/com/ytech/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytech.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
